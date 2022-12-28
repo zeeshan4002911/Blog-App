cd94956d6b0af5aa05ecc6c6254bea982f9bbb9f
AAA4002911 <zeeshan4002911@gmail.com>
2022-12-28 22:10:57 +0530

    routes added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Login from "./component/Login";
 import Register from "./component/Register";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./component/Home";
+import Create from "./component/Create";
 
 function App() {
   return (
@@ -10,6 +13,8 @@ function App() {
         <Routes>
           <Route index path='/' element={<Login />} />
           <Route path='/register' element={<Register />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/create' element={<Create />} />
         </Routes>
       </BrowserRouter>
     </>
