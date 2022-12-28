87e2e5db099703175dc6ec72e9d5ef67418b012e
AAA4002911 <zeeshan4002911@gmail.com>
2022-12-28 21:01:44 +0530

    API url fixed

--- client/src/component/Register.jsx
+++ client/src/component/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-const URL = process.env.API_KEY || "http://localhost:3001"
+const URL = process.env.REACT_APP_API_KEY || "http://localhost:3001"
 
 const Register = () => {
     const navigate = useNavigate();
