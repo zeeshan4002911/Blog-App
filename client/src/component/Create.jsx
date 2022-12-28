7798b727c9d6fa43de7495776b0c115bc433abb1
AAA4002911 <zeeshan4002911@gmail.com>
2022-12-28 22:11:16 +0530

    initial commit

---
+++ client/src/component/Create.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+const URL = process.env.REACT_APP_API_KEY || "http://localhost:3001"
